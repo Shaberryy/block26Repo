c74222531f8ed6b7b641bdb8de348094e413279b
shaberry <jaden.png02@gmail.com>
2023-08-07 11:06:35 -0700

    Contact list

--- Contact-List/src/App.jsx
+++ Contact-List/src/App.jsx
@@ -1,4 +1,38 @@
 // import { useState } from 'react'
+// import React from "react";
+import "./App.css";
+import ContactList from './contactlist';
 
-import './App.css'
-
+// export default function ContactList() {
+//   return (
+//     <>
+//       <table>
+//         <thead>
+//           {/* <h1>Contact List</h1> */}
+//           <tr>
+//             <th colSpan="3">Contact List</th>
+//           </tr>
+//         </thead>
+//       <tbody>
+//         <tr>
+//           <td>Name</td>
+//           <td>Email</td>
+//           <td>Phone</td>
+//         </tr>
+//         {
+//             // map data 
+//         }
+//       </tbody>
+//       </table>
+      
+//     </>
+//   );
+// }
+export default function App() {
+    return(
+    <>
+    <ContactList/>
+    </>
+    );
+}
+// export default App
